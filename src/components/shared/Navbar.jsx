56837b5b65771834627a82dcd381cee89d55197f
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-23 11:24:45 +0530

    first commit

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,33 @@
+import { IoIosSearch } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between mx-3 h-16">
+      <div className="flex items-center gap-10">
+        <div className="flex items-center gap-2">
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <RxHamburgerMenu size={"20px"} />
+          </div>
+          <img
+            className="w-8"
+            src="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png"
+            alt="gmail-logo"
+          />
+          <h1 className="text-2xl text-gray-500 font-medium ">Gmail</h1>
+        </div>
+      </div>
+      <div className="md:block hidden w-[50%] mr-60">
+        <div className="flex items-center bg-[#EAF1FB] px-2 py-3 rounded-full">
+          <IoIosSearch size={"24px"} className="text-gray-700" />
+          <input
+            type="text"
+            className="rounded-full w-full bg-transparent outline-none px-1"
+            placeholder="Search mail"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
