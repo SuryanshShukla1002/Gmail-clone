2eda4c0568de9c1078b93ca8ba9cf0d6322782c5
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-24 11:46:57 +0530

    using map to make all options of Gmail

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,28 @@
 import { LuPencil } from "react-icons/lu";
 
+const siderbarItems = [
+  {
+    icon: <LuPencil size={"24px"} />,
+    text: "Inbox",
+  },
+  {
+    icon: <LuPencil size={"24px"} />,
+    text: "starred",
+  },
+  {
+    icon: <LuPencil size={"24px"} />,
+    text: "Snoozed",
+  },
+  {
+    icon: <LuPencil size={"24px"} />,
+    text: "Sent",
+  },
+  {
+    icon: <LuPencil size={"24px"} />,
+    text: "Drafts",
+  },
+];
+
 const Sidebar = () => {
   return (
     <div className="w-[15%]">
@@ -8,6 +31,16 @@ const Sidebar = () => {
           <LuPencil size={"24px"} />
           Compose
         </button>
+      </div>
+      <div className="text-gray-500">
+        {siderbarItems.map((item, index) => {
+          return (
+            <div className="flex items-center gap-4 pl-6 py-1 rounded-r-full hover:cursor-pointer hover:bg-gray-200  my-2">
+              {item.icon}
+              <p>{item.text}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
