375a1d9ef9bab9b31f42143cc9860f7d9ebedefe
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-25 07:52:30 +0530

    all single message import

---
+++ src/components/Messages.jsx
@@ -0,0 +1,20 @@
+import Message from './Message';
+
+const Messages = () => {
+  return (
+    <div>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+    </div>
+  )
+}
+export default Messages
