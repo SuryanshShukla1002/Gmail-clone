29ddc5fed34bac0edec1c3aca9368abebba0fa60
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-25 07:49:01 +0530

    router provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import './App.css'
-import Navbar from './components/shared/Navbar';
-import Sidebar from './components/Sidebar';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/shared/Navbar";
+import Sidebar from "./components/Sidebar";
+import Body from "./components/Body";
+import Inbox from "./components/Inbox";
+import Mail from "./components/Mail";
 function App() {
-
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body />,
+      children: [
+        {
+          path: "/",
+          element: <Inbox />,
+        },
+        {
+          path: "/mail/:id",
+          element: <Mail />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className='bg-[#F6F8FC] h-screen w-screen overflow-hidden'>
-     <Navbar/>
-     <Sidebar/>
+    <div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
+      <Navbar />
+      <RouterProvider router={router}/>
     </div>
-  )
+  );
 }
 
-export default App
- 
+export default App;
