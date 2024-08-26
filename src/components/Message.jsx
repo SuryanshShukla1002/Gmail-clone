1895c28357a94e8bf01121d7ac3cbee1f62648b6
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-26 18:03:43 +0530

    open mail path setup

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,18 @@
 import { MdCropSquare } from "react-icons/md";
-import { RiStarLine } from 'react-icons/ri';
+import { RiStarLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
 const Message = () => {
+  const navigate = useNavigate();
+  const openMail = () => {
+    navigate("/mail/12364r6734r");
+  };
+
   return (
-    <div className="flex items-start justify-between border-b border-gray-200 px-4 text-sm hover:cursor-pointer hover:shadow-md py-3">
+    <div
+      onClick={openMail}
+      className="flex items-start justify-between border-b border-gray-200 px-4 text-sm hover:cursor-pointer hover:shadow-md py-3"
+    >
       <div className="flex items-center gap-3">
         <div className="flex-none text-gray-300">
           <MdCropSquare className="w-5 h-5" />
@@ -12,12 +21,13 @@ const Message = () => {
           <RiStarLine className="w-5 h-5" />
         </div>
       </div>
-      <div className='flex-1 ml-4'>
-        <p className='text-gray-600 truncate inline-block max-w-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse alias doloremque in numquam minus ea.</p>
+      <div className="flex-1 ml-4">
+        <p className="text-gray-600 truncate inline-block max-w-full">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse alias
+          doloremque in numquam minus ea.
+        </p>
       </div>
-      <div className='flex-none text-gray-400 text-sm'>
-        time ayega
-      </div>
+      <div className="flex-none text-gray-400 text-sm">time ayega</div>
     </div>
   );
 };
