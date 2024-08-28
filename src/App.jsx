6fb44201613bc31eb1b13efaf20a80ed86834bc9
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-28 13:50:59 +0530

    sendMail mounted compose

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Sidebar from "./components/Sidebar";
 import Body from "./components/Body";
 import Inbox from "./components/Inbox";
 import Mail from "./components/Mail";
+import SendMail from './components/SendMail';
 function App() {
   const router = createBrowserRouter([
     {
@@ -26,6 +27,9 @@ function App() {
     <div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
       <Navbar />
       <RouterProvider router={router}/>
+      <div className='absolute w-[30%] bottom-0 right-20 z-10'>
+        <SendMail/>
+      </div>
     </div>
   );
 }
