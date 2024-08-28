946a3e99bb49e88453f75b1ae834b4c353890999
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-28 13:53:57 +0530

    useDispatch the compose button to open

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+import { IoMdStar } from "react-icons/io";
 import { LuPencil } from "react-icons/lu";
+import {
+  MdOutlineDrafts,
+  MdOutlineKeyboardArrowDown,
+  MdOutlineWatchLater,
+} from "react-icons/md";
+import { TbSend2 } from "react-icons/tb";
+import { useDispatch } from 'react-redux';
+import { setOpen } from '../redux/appSlice';
 
 const siderbarItems = [
   {
@@ -6,28 +16,34 @@ const siderbarItems = [
     text: "Inbox",
   },
   {
-    icon: <LuPencil size={"24px"} />,
+    icon: <IoMdStar size={"24px"} />,
     text: "starred",
   },
   {
-    icon: <LuPencil size={"24px"} />,
+    icon: <MdOutlineWatchLater size={"24px"} />,
     text: "Snoozed",
   },
   {
-    icon: <LuPencil size={"24px"} />,
+    icon: <TbSend2 size={"24px"} />,
     text: "Sent",
   },
   {
-    icon: <LuPencil size={"24px"} />,
+    icon: <MdOutlineDrafts size={"24px"} />,
     text: "Drafts",
+  },
+  {
+    icon: <MdOutlineKeyboardArrowDown size={"24px"} />,
+    text: "More",
   },
 ];
 
 const Sidebar = () => {
+  // const [open, setOpen] = useState(false)
+  const dispatch = useDispatch()
   return (
     <div className="w-[15%]">
       <div className="p-3">
-        <button className="flex items-center gap-2 p-4 rounded-2xl hover:shadow-md bg-[#C2E7FF] ">
+        <button onClick={() => dispatch(setOpen(true))} className="flex items-center gap-2 p-4 rounded-2xl hover:shadow-md bg-[#C2E7FF] ">
           <LuPencil size={"24px"} />
           Compose
         </button>
