c900c252c926e751964708d3c1a9add39601d6b4
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-29 15:13:36 +0530

    opening mail message functinality done

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -11,13 +11,19 @@ import {
   MdOutlineDriveFileMove,
 } from "react-icons/md";
 import { BiArchiveIn } from "react-icons/bi";
+import { useSelector } from 'react-redux';
 
 const Mail = () => {
+  const navigate = useNavigate();
+  const {selectedEmail} = useSelector(store=>store.appSlice)
   return (
     <div className="flex-1 bg-white rounded-xl mx-5">
       <div className="flex items-center justify-between px-4">
         <div className="flex items-center gap-2 text-gray-700 py-2">
-          <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
+          <div
+            onClick={() => navigate("/")}
+            className="p-2 rounded-full hover:bg-gray-100 cursor-pointer"
+          >
             <IoMdArrowBack size={"20px"} />
           </div>
           <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
@@ -57,19 +63,19 @@ const Mail = () => {
       <div className="h-[90vh] overflow-y-auto p-4">
         <div className="flex items-center justify-between bg-white gap-1">
           <div className="flex items-center gap-2">
-            <h1 className="text-xl font-medium">Subject</h1>
+            <h1 className="text-xl font-medium">{selectedEmail?.subject}</h1>
             <span className="text-sm bg-gray-200 rounded-md px-2">inbox</span>
           </div>
           <div className="flex-none text-gray-400 my-5 text-sm">
-            <p>12-08-2024</p>
+            <p>{new Date(selectedEmail?.createdAt?.seconds*1000).toUTCString()}</p>
           </div>
         </div>
         <div className="text-gray-500 text-sm">
-          <h1>to</h1>
+          <h1>{selectedEmail?.to}</h1>
           <span>to me</span>
         </div>
         <div className="my-10">
-          <p>message</p>
+          <p>{selectedEmail?.message}</p>
         </div>
       </div>
     </div>
