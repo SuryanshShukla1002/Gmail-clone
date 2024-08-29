505b19e7b41bbb2457a70bd98dda11caeab65442
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-29 11:54:06 +0530

    onsnapshot added for data retrived

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
 import Message from './Message';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Messages = () => {
+
+  useEffect(() => {
+    // for sorting of emails 
+    const q = query(collection(db, "emails"), )
+    const unsubscribe = onSnapshot(collection(db, "emails"), (snapshot) => {
+      const allEmails = snapshot.docs.map((doc) => ({...doc.data(), id:doc.id}))
+      console.log(allEmails);
+    })
+    // cleanup
+    return () => unsubscribe();
+  },[]);
   return (
     <div>
-        <Message/>
-        <Message/>
         <Message/>
         <Message/>
         <Message/>
