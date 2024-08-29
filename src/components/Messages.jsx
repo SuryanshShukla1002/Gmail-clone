99830bbe73e4eeab98c5274d58862e92bc4898bd
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-29 12:02:57 +0530

    query added for sorting of mails send per each

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import Message from './Message';
-import { collection, onSnapshot, query } from 'firebase/firestore';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import { db } from '../firebase';
 
 const Messages = () => {
 
   useEffect(() => {
     // for sorting of emails 
-    const q = query(collection(db, "emails"), )
-    const unsubscribe = onSnapshot(collection(db, "emails"), (snapshot) => {
+    const q = query(collection(db, "emails"), orderBy('createdAt', 'desc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
       const allEmails = snapshot.docs.map((doc) => ({...doc.data(), id:doc.id}))
       console.log(allEmails);
     })
