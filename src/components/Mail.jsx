ec60b21514924e23e62ad48dbd022b8206a75836
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-08-30 12:25:05 +0530

    delete function added for email

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -12,10 +12,22 @@ import {
 } from "react-icons/md";
 import { BiArchiveIn } from "react-icons/bi";
 import { useSelector } from 'react-redux';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Mail = () => {
   const navigate = useNavigate();
-  const {selectedEmail} = useSelector(store=>store.appSlice)
+  const params = useParams()
+  const {selectedMail} = useSelector(store=>store.appSlice)
+  const deleteMailById = async(id) => {
+    try{
+      await deleteDoc(doc(db, "emails", id))
+      navigate("/")
+    }catch(error){
+      console.log(error);
+      
+    }
+  }
   return (
     <div className="flex-1 bg-white rounded-xl mx-5">
       <div className="flex items-center justify-between px-4">
@@ -32,7 +44,7 @@ const Mail = () => {
           <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
             <MdOutlineReport size={"20px"} />
           </div>
-          <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
+          <div  onClick={()=>deleteMailById(params.id)} className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
             <MdDeleteOutline size={"20px"} />
           </div>
           <div className="p-2 rounded-full hover:bg-gray-100 cursor-pointer">
@@ -63,19 +75,19 @@ const Mail = () => {
       <div className="h-[90vh] overflow-y-auto p-4">
         <div className="flex items-center justify-between bg-white gap-1">
           <div className="flex items-center gap-2">
-            <h1 className="text-xl font-medium">{selectedEmail?.subject}</h1>
+            <h1 className="text-xl font-medium">{selectedMail?.subject}</h1>
             <span className="text-sm bg-gray-200 rounded-md px-2">inbox</span>
           </div>
           <div className="flex-none text-gray-400 my-5 text-sm">
-            <p>{new Date(selectedEmail?.createdAt?.seconds*1000).toUTCString()}</p>
+            <p>{new Date(selectedMail?.createdAt?.seconds*1000).toUTCString()}</p>
           </div>
         </div>
         <div className="text-gray-500 text-sm">
-          <h1>{selectedEmail?.to}</h1>
+          <h1>{selectedMail?.to}</h1>
           <span>to me</span>
         </div>
         <div className="my-10">
-          <p>{selectedEmail?.message}</p>
+          <p>{selectedMail?.message}</p>
         </div>
       </div>
     </div>
