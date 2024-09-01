b344441fa00fa9a1ea2b34272c79c78001d29109
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-09-01 08:30:34 +0530

    search text state handled

--- src/redux/appSlice.js
+++ src/redux/appSlice.js
@@ -1,16 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const appSlice = createSlice({
-    name:"appSlice",
-    initialState:{
-        open:false
+    name: "appSlice",
+    initialState: {
+        open: false,
+        emails: [],
+        selectedMail: null,
+        SearchText:""
     },
-    reducers:{
-        setOpen:(state,action)=> {
+    reducers: {
+        setOpen: (state, action) => {
             state.open = action.payload;
+        },
+        setEmails: (state, action) => {
+            state.emails = action.payload;
+        },
+        setselectedMail: (state, action) => {
+            state.selectedMail = action.payload;
+        },
+        setSearchText:(state, action)=> {
+            state.SearchText = action.payload
         }
     }
-})
+});
 
-export const {setOpen} = appSlice.actions
-export default appSlice.reducer
+export const { setOpen, setEmails, setselectedMail, setSearchText } = appSlice.actions;
+export default appSlice.reducer;
