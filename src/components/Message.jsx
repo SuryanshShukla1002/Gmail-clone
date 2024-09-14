cfc2ea5ebf0e88622b31faea1840517cba8b1ae2
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-09-14 12:50:13 +0530

    framer mostion added

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,23 +1,23 @@
 import { MdCropSquare } from "react-icons/md";
 import { RiStarLine } from "react-icons/ri";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import {  setselectedMail } from '../redux/appSlice';
-import { motion } from 'framer-motion';
+import { setselectedMail } from "../redux/appSlice";
+import { motion } from "framer-motion";
 
-const Message = ({email}) => {
+const Message = ({ email }) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const openMail = () => {
-    dispatch(setselectedMail(email))
+    dispatch(setselectedMail(email));
     navigate(`/mail/${email.id}`);
   };
 
   return (
     <motion.div
-    initial={{opacity:0, y:20}}
-    animate={{opacity:1, y:0}}
-    transition={{duration:0.5}}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
       onClick={openMail}
       className="flex items-start justify-between border-b border-gray-200 px-4 text-sm hover:cursor-pointer hover:shadow-md py-3"
     >
@@ -28,17 +28,18 @@ const Message = ({email}) => {
         <div className="flex-none text-gray-300">
           <RiStarLine className="w-5 h-5" />
         </div>
+        <div>
+          <h1 className="font-semibold">{email?.to}</h1>
+        </div>
       </div>
       <div className="flex-1 ml-4">
         <p className="text-gray-600 truncate inline-block max-w-full">
-         {email?.message}
+          {email?.message}
         </p>
       </div>
       <div className="flex-none text-gray-400 text-sm">
-        <p>
-        {new Date(email?.createdAt?.seconds*1000).toUTCString()}
-        </p>
-        </div>
+        <p>{new Date(email?.createdAt?.seconds * 1000).toUTCString()}</p>
+      </div>
     </motion.div>
   );
 };
