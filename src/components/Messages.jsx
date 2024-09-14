63cb16a540c93a60588295c9a4e14f50385cb576
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-09-14 12:50:53 +0530

    final commit push

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,39 +1,43 @@
-import { useEffect, useState } from 'react';
-import Message from './Message';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { db } from '../firebase';
-import { useDispatch, useSelector } from 'react-redux';
-import {setEmails} from '../redux/appSlice'
+import { useEffect, useState } from "react";
+import Message from "./Message";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { setEmails } from "../redux/appSlice";
 
 const Messages = () => {
-  const {SearchText,emails} = useSelector(store=> store.appSlice)
-const dispatch = useDispatch()
-const [temEmails, setTemEmails] = useState(emails)
+  const { SearchText, emails } = useSelector((store) => store.appSlice);
+  const dispatch = useDispatch();
+  const [temEmails, setTemEmails] = useState(emails);
   useEffect(() => {
-    // for sorting of emails 
-    const q = query(collection(db, "emails"), orderBy('createdAt', 'desc'))
+    // for sorting of emails
+    const q = query(collection(db, "emails"), orderBy("createdAt", "desc"));
     const unsubscribe = onSnapshot(q, (snapshot) => {
-      const allEmails = snapshot.docs.map((doc) => ({...doc.data(), id:doc.id}))
+      const allEmails = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
       // console.log(allEmails);
-      dispatch(setEmails(allEmails))
-    })
+      dispatch(setEmails(allEmails));
+    });
     // cleanup
     return () => unsubscribe();
-  },[]);
+  }, []);
   useEffect(() => {
     const filteredEmail = emails?.filter((email) => {
-      return email?.subject?.toLowerCase().includes(SearchText.toLowerCase()) || email?.to?.toLowerCase().includes(SearchText.toLowerCase()) || email?.message?.toLowerCase().includes(SearchText.toLowerCase())
-    })
-    setTemEmails(filteredEmail)
-  },[SearchText, emails])
+      return (
+        email?.subject?.toLowerCase().includes(SearchText.toLowerCase()) ||
+        email?.to?.toLowerCase().includes(SearchText.toLowerCase()) ||
+        email?.message?.toLowerCase().includes(SearchText.toLowerCase())
+      );
+    });
+    setTemEmails(filteredEmail);
+  }, [SearchText, emails]);
 
   return (
     <div>
-      {
-        temEmails && temEmails?.map((email) =>  <Message email={email}/>)
-      }
-        
+      {temEmails && temEmails?.map((email) => <Message email={email} />)}
     </div>
-  )
-}
-export default Messages
+  );
+};
+export default Messages;
